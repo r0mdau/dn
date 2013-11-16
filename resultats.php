be1aad15852a68f85237f178b4385187bbce3eb1
Romain Dauby <romain.dauby@gmail.com>
2013-11-16 20:41:23 +0100

    Issue 3 : Seconde page pour afficher les résultats

---
+++ resultats.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta charset="utf-8">
+        <title>Destinées Numériques</title>
+        <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-6 col-md-4"></div>
+                <div class="col-xs-6 col-md-4">
+                    <h2 class="align-center">Ils ont adhéré</h2><br>
+                    <div id="liste" class="well">
+                        
+                    </div>
+                </div>
+                <div class="col-xs-6 col-md-4"></div>
+            </div>
+        </div>
+    </body>
+</html>
