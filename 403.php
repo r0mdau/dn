092edb32ca09f2ed8e78d0619bb23c58733924d2
Romain Dauby <romain.dauby@gmail.com>
2013-11-16 19:29:22 +0100

    Pages d'erreurs HTTP

---
+++ 403.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta charset="utf-8">
+        <title>Adhésion</title>
+        <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-6 col-md-4"></div>
+                <div class="col-xs-6 col-md-4">
+                    Hey, vous n'avez pas le droit de naviguer dans ce dossier !
+                </div>
+                <div class="col-xs-6 col-md-4"></div>
+            </div>
+        </div>  
+    </body>
+</html>
