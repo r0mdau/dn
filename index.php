414fd616eb5d66cdaa0b2c32d46e4548782795fa
Romain Dauby <romain.dauby@gmail.com>
2013-11-16 17:36:54 +0100

    Faire le ménage à la racine : bootstrap dans un dossier lib

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <meta charset="utf-8">
         <title>Adhésion</title>
-        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
     </head>
     <body>
         <div class="container">
