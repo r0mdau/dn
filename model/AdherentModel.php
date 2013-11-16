5046f292cbe0e48ee24397021d6640e2d382d174
Romain Dauby <romain.dauby@gmail.com>
2013-11-16 20:59:15 +0100

    Issue 5 : 1 - Récupérer le nombre d'adhérents

--- model/AdherentModel.php
+++ model/AdherentModel.php
@@ -5,4 +5,9 @@
             return Db::query('INSERT INTO adherent (prenom, nom, id_entreprise)
                       VALUES (\''.$adherent->getNom().'\', \''.$adherent->getPrenom().'\', \''.$adherent->getIdEntreprise().'\')');
         }
+        
+        public static function nombreTotal(){
+            $res = Db::querySingle('SELECT COUNT(*) nb FROM adherent');
+            return isset($res->nb) ? $res->nb : 0;
+        }
     }
--- test/model/AdherentModelTest.php
+++ test/model/AdherentModelTest.php
@@ -21,6 +21,17 @@ class AdherentModelTest extends PHPUnit_Framework_TestCase {
         $this->assertTrue(AdherentModel::add($this->adherent));
     }
     
+    public function testJePeuxRecupererLeNombreDAdherents(){
+        EntrepriseModel::add($this->entreprise);
+        $idEntreprise = EntrepriseModel::getLastId();
+        $this->adherent->setIdEntreprise($idEntreprise);
+        AdherentModel::add($this->adherent);
+        $this->assertEquals(1, AdherentModel::nombreTotal());
+        
+        AdherentModel::add($this->adherent);
+        $this->assertEquals(2, AdherentModel::nombreTotal());
+    }
+    
     private $adherent;
     private $entreprise;
 }
