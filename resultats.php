e56a59bb59ed669946f682613b48e85dc3619684
Romain Dauby <romain.dauby@gmail.com>
2013-11-17 21:34:43 +0100

    Issue 18 : Actualiser toutes les secondes le compteur d'adhérents

--- resultats.php
+++ resultats.php
@@ -5,28 +5,47 @@
         <meta charset="utf-8">
         <title>Destinées Numériques</title>
         <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="lib/flipclock/css/flipclock.css">
     </head>
     <body>
         <div class="container">
             <div class="row">
                 <div class="col-xs-6 col-md-4"></div>
-                <div class="col-xs-6 col-md-4">
-                    <h2 class="align-center">Ils ont adhéré</h2><br>
+                <div class="col-xs-6 col-md-4" class="text-center">
+                    <h2 class="align-center">Ils ont adhéré</h2>                    
+                    <br>
                     <div id="liste" class="well">
                         
                     </div>
-                    <div id="compteur">
+                    <div id="compteur" class="text-center">
                         
                     </div>
                 </div>
                 <div class="col-xs-6 col-md-4"></div>
             </div>
         </div>
-        <script src="lib/jquery/js/jquery-1.10.2.min.js"></script>
+        <script src="lib/jquery/js/jquery-1.10.2.min.js"></script>        		
+        <script src="lib/flipclock/js/libs/base.js"></script>
+        <script src="lib/flipclock/js/flipclock.js"></script>
+        <script src="lib/flipclock/js/faces/counter.js"></script>	
         <script>
+            var clock;
             $(document).ready(function(){
-                $('#compteur').html(3);
+                clock = $('#compteur').FlipClock(0, {
+                        clockFace: 'Counter'
+                });
+                
+                actualiserCompteur();
             });
+            
+            function actualiserCompteur(){                
+                $.ajax({
+                    url : "ajaj/compteur.php"                    
+                }).done(function(res){
+                    clock.setValue(res);
+                });
+                setTimeout(actualiserCompteur, 1000);
+            }
         </script>
     </body>
 </html>
