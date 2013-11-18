db8d5bbef0d7671ddb7e03abfb55625a1823d391
Romain Dauby <romain.dauby@gmail.com>
2013-11-18 18:14:58 +0100

    Add exemple du fichier de configuration

---
+++ settings.php
@@ -0,0 +1,7 @@
+<?php
+    define('_DB_SERVER_', 'localhost');
+    define('_DB_NAME_', 'DATABASE');
+    define('_DB_USER_', 'USER');
+    define('_DB_PASSWD_', 'PASSWORD');
+    //define('_DB_FIRST_QUERY_', 'SET NAMES UTF8');
+?>
