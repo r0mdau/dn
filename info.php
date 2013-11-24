3417dbb157b67b8028dee619937cb8ae1ea01cec
Romain Dauby <romain.dauby@gmail.com>
2013-11-24 12:35:37 +0100

    Update info.php : responsive design

--- info.php
+++ info.php
@@ -3,14 +3,14 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <meta charset="utf-8">
-        <title>Informations</title>
+        <title>Destinées Numériques</title>
         <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="lib/dn/css/dn.css">
     </head>
     <body>
         <div class="container">
             <div class="row">
-                <div class="col-xs-12 col-sm-4 col-md-offset-4">
+                <div class="col-xs-12 col-sm-4 col-md-8 col-md-offset-2">
                     <img src="lib/dn/img/Logo_DN3.jpg" alt="Logo destinées numériques" class="img-responsive">
                     <?php
                         if($_SERVER['REQUEST_URI'] == '/merci')
@@ -21,9 +21,9 @@
                             header('Location:accueil');
                             exit;
                         }
-                    ?>                    
+                    ?>
                 </div>
             </div>
-        </div>  
+        </div>
     </body>
 </html>
